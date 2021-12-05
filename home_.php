2e957bd45b0500d3ff8fd9c7369a094c5f8ee909
phamnga <phamnga.bibi89@gmail.com>
2021-12-05 21:18:39 +0700

    first commit

---
+++ home_.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="icon" href="../img/img3.jpeg" type="image/png">
+    <!-- css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="../js/main.js"></script>
+</head>
+<body>
+    <header class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                      <a class="navbar-brand" href="#"><i class="material-icon bi bi-person-circle"></i>Phuong Nga Pham</a>
+                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                          <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="../index/home_.html">Home</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="../index/post_.html">Featured</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link  "href="../index/view_.html">View Post</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </nav>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class=" row spacerow">
+                <div class="title">
+                    <h4>Pham Thi Phuong Nga</h4>
+                    <p>Your Professtion</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="../img/img1.jpeg"style="width: 100%;" class="img-fluid rounded" alt="...">
+                </div>
+                <div class="col-md-6">
+                    <h4>About</h4>
+                    <p class="text-img">As covid-19 has spread around the world, people have become grimly familiar with the death tolls 
+                        that their governments publish each day. Unfortunately, the total number of fatalities caused by the pandemic may 
+                        be even higher, for several reasons. First, the official statistics in many countries exclude victims who did not 
+                        test positive for coronavirus before dying—which can be a substantial majority in places with little capacity for 
+                        testing. Second, hospitals and civil registries may not process death certificates for several days, or even weeks, 
+                        which creates lags in the data. And third, the pandemic has made it harder for doctors to treat other conditions and
+                        discouraged people from going to hospital, which may have indirectly caused an increase in fatalities from diseases
+                        other than covid-19.</p>
+                </div>
+            </div>
+            <div class="row spacerow">
+                <div class="title">
+                    <h4>Function</h4>
+                </div>
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <img src="../img/img2.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        <figcaption class="figure-caption">A caption for the above image.</figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <img src="../img/img3.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        <figcaption class="figure-caption">A caption for the above image.</figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <img src="../img/img5.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        <figcaption class="figure-caption">A caption for the above image.</figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <img src="../img/img6.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        <figcaption class="figure-caption">A caption for the above image.</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div class="row spacerow" style="margin: 0 20px 0 20px;">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h5>Title 1</h5>
+                                <p>As covid-19 has spread around the world, people have become grimly familiar with the death tolls 
+                                    that their governments publish each day. Unfortunately, the total number of fatalities caused by the pandemic may 
+                                    be even higher, for several reasons. First, the official statistics in many countries exclude victims who did not 
+                                    test positive for coronavirus before dying—which can be a substantial majority in places with little capacity for 
+                                    testing.
+                                </p>
+                            </div>
+                            <div class="col-md-6">
+                                <h5>Title 2</h5>
+                                <p>As covid-19 has spread around the world, people have become grimly familiar with the death tolls 
+                                    that their governments publish each day. Unfortunately, the total number of fatalities caused by the pandemic may 
+                                    be even higher, for several reasons. First, the official statistics in many countries exclude victims who did not 
+                                    test positive for coronavirus before dying—which can be a substantial majority in places with little capacity for 
+                                    testing.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                  <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h5>Title 3</h5>
+                            <p>As covid-19 has spread around the world, people have become grimly familiar with the death tolls 
+                                that their governments publish each day. Unfortunately, the total number of fatalities caused by the pandemic may 
+                                be even higher, for several reasons. First, the official statistics in many countries exclude victims who did not 
+                                test positive for coronavirus before dying—which can be a substantial majority in places with little capacity for 
+                                testing.
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <h5>Title 4</h5>
+                            <p>As covid-19 has spread around the world, people have become grimly familiar with the death tolls 
+                                that their governments publish each day. Unfortunately, the total number of fatalities caused by the pandemic may 
+                                be even higher, for several reasons. First, the official statistics in many countries exclude victims who did not 
+                                test positive for coronavirus before dying—which can be a substantial majority in places with little capacity for 
+                                testing.
+                            </p>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h5>Title 5</h5>
+                            <p>As covid-19 has spread around the world, people have become grimly familiar with the death tolls 
+                                that their governments publish each day. Unfortunately, the total number of fatalities caused by the pandemic may 
+                                be even higher, for several reasons. First, the official statistics in many countries exclude victims who did not 
+                                test positive for coronavirus before dying—which can be a substantial majority in places with little capacity for 
+                                testing.
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <h5>Title 6</h5>
+                            <p>As covid-19 has spread around the world, people have become grimly familiar with the death tolls 
+                                that their governments publish each day. Unfortunately, the total number of fatalities caused by the pandemic may 
+                                be even higher, for several reasons. First, the official statistics in many countries exclude victims who did not 
+                                test positive for coronavirus before dying—which can be a substantial majority in places with little capacity for 
+                                testing.
+                            </p>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        <div class="container">
+            <div class="contact spacerow">
+                <h4>Contact (Your Name)</h4>
+                <form method="post" action="" onsubmit="return KiemtraForm()" name="frmsubmit">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <label for="exampleInputEmail1" class="form-label">First Name</label>
+                            <input type="text" name="firstname" class="form-control" id="firstname" aria-describedby="emailHelp">
+                            <p style="color:red;"id="baoloi1"></p>
+                            <!-- <p style="color:green;"id="tbao1"></p> -->
+                        </div>
+                        <div class="col-md-4">
+                            <label for="exampleInputPassword1" class="form-label">Last Name</label>
+                            <input type="text" name="lastname" class="form-control" id="lastname">
+                            <p style="color:red;"id="baoloi2"></p>
+                            <!-- <p style="color:green;"id="tbao2"></p> -->
+                        </div>
+                        <div class="col-md-4">
+                            <label for="basic-url" class="form-label">UserName</label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon3">@</span>
+                                <input type="text" name="username"class="form-control" id="username" aria-describedby="basic-addon3">
+                            </div>
+                            <p style="color:red; "id="baoloi3"></p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="exampleInputEmail1" class="form-label">City</label>
+                            <input type="text" name="city" class="form-control" id="city" aria-describedby="emailHelp">
+                            <p style="color:red; " id="baoloi4"></p>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col-md-6" >
+                                    <label for="exampleInputPassword1" class="form-label">State</label>
+                                    <div class="input-group">
+                                        <select class="form-select" id="state" aria-label="Example select with button addon">
+                                          <option selected>Choose...</option>
+                                          <option name="state" value="1">One</option>
+                                          <option name="state" value="2">Two</option>
+                                          <option name="state" value="3">Three</option>
+                                        </select>
+                                        <p style="color:red; " id="baoloi5"></p>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-md-6">
+                                    <label for="basic-url" class="form-label form-control" style="margin-bottom: 0;">ZIP</label>
+                                    <div class="input-group">
+                                        <input type="text" name="zip"class="form-control" id="zip" aria-describedby="basic-addon3">
+                                    </div>
+                                    <p style="color:red; "id="baoloi6"></p>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" name="agree" class="form-check-input" id="agree">
+                            <label class="form-check-label" for="exampleCheck1">Agreed</label>
+                            <p style="color:red; "id="baoloi7"></p>
+                            </div>
+                            <button type="submit" class="btn btn-primary"style="width: 10%;">Submit Form</button>
+                           
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+    <footer class="bg-footer spacerow">
+        <div class="container" style="padding:40px 0 40px 0;">
+            <div class="row" >
+                <div class="col-md-3">
+                    <h5><span class=" material-icon bi bi-twitter"></span>Footer Item</h5>
+                    <div>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <h5><span class=" material-icon bi bi-twitter"></span>Footer Item</h5>
+                    <div>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <h5><span class=" material-icon bi bi-twitter"></span>Footer Item</h5>
+                    <div>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <h5><span class=" material-icon bi bi-twitter"></span>Footer Item</h5>
+                    <div>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                        <p><a href="">Item</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="banner-footer">
+                <p style="text-align: center;margin: 10px 0 10px 0;color: gray;"><i class="bi bi-flower1"></i>Phạm Thị Phương Nga<i class="bi bi-flower1"></i></p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
